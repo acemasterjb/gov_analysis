2f98e86b29428f3ab857b4081c2e0b3f564414e7
Jamil Bousquet <jamil.bousquet@gmail.com>
2023-01-26 15:23:21 -0400

    :bug: Fix votes not being reaggregated issue

--- libs/data_processing/reaggregation.py
+++ libs/data_processing/reaggregation.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
 
+def get_whales(unfiltered_propsal: pd.DataFrame, top_shareholders: pd.Series) -> pd.DataFrame:
+    return unfiltered_propsal.loc[
+        lambda df: [voter in top_shareholders.values for voter in df["voter"]]
+    ]
+
+
 def reaggregate_votes_single_choice_or_basic(
     unfiltered_propsal: pd.DataFrame, top_shareholders: pd.Series
 ) -> pd.DataFrame | None:
-    whales: pd.DataFrame = unfiltered_propsal.loc[
-        lambda df: [voter in top_shareholders for voter in df["voter"]]
-    ]
+    whales: pd.DataFrame = get_whales(unfiltered_propsal, top_shareholders)
     if whales.empty:
         return whales
     for _, whale in whales.iterrows():
-        scores = unfiltered_propsal["proposal_scores"].iloc[0]
+        scores: list[float | int] = unfiltered_propsal["proposal_scores"].iloc[0]
         scores[whale["choice"] - 1] -= whale["vp"]
         unfiltered_propsal["proposal_scores"] = [scores] * unfiltered_propsal.shape[0]
 
@@ -20,17 +24,14 @@ def reaggregate_votes_single_choice_or_basic(
 def reaggregate_votes_approval(
     unfiltered_propsal: pd.DataFrame, top_shareholders: pd.Series
 ):
-    whales: pd.DataFrame = unfiltered_propsal.loc[
-        lambda df: [voter in top_shareholders for voter in df["voter"]]
-    ]
+    whales: pd.DataFrame = get_whales(unfiltered_propsal, top_shareholders)
     if whales.empty:
         return whales
     for _, whale in whales.iterrows():
-        scores = unfiltered_propsal["proposal_scores"].iloc[0]
+        scores: list[float | int] = unfiltered_propsal["proposal_scores"].iloc[0]
         new_scores = [score - whale["vp"] for score in scores]
-        unfiltered_propsal["proposal_scores"] = [new_scores] * unfiltered_propsal.shape[
-            0
-        ]
+        n_rows = unfiltered_propsal.shape[0]
+        unfiltered_propsal["proposal_scores"] = [new_scores] * n_rows
 
     return unfiltered_propsal
 
@@ -38,9 +39,7 @@ def reaggregate_votes_approval(
 def reaggregate_votes_weighted(
     unfiltered_propsal: pd.DataFrame, top_shareholders: pd.Series
 ):
-    whales: pd.DataFrame = unfiltered_propsal.loc[
-        lambda df: [voter in top_shareholders for voter in df["voter"]]
-    ]
+    whales: pd.DataFrame = get_whales(unfiltered_propsal, top_shareholders)
     if whales.empty:
         return whales
     for _, whale in whales.iterrows():
@@ -51,8 +50,7 @@ def reaggregate_votes_weighted(
             (weight / weight_total) * whale["vp"] for weight in weights
         ]
         new_scores = [score - weight for score, weight in zip(scores, weight_values)]
-        unfiltered_propsal["proposal_scores"] = [new_scores] * unfiltered_propsal.shape[
-            0
-        ]
+        n_rows = unfiltered_propsal.shape[0]
+        unfiltered_propsal["proposal_scores"] = [new_scores] * n_rows
 
     return unfiltered_propsal
